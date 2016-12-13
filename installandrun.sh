af9fb541efe43084c39aaee4de7b23b7b6c73e98
Wyatt Preul <wpreul@gmail.com>
2016-12-13 10:58:33 -0600

    Use nvm

--- installandrun.sh
+++ installandrun.sh
@@ -1,13 +1,10 @@
+yum update
 yum install apache2-utils
-yum install npm
 npm install hapi
 npm install express
 npm install restify
 npm install koa
-npm install n
 npm install total.js
-n install latest
-n latest
 
 echo express >> results.txt ;
 node expressserver.js & sleep 5 ; ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
